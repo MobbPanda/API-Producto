7df9838478a0eedd33d51008106602baa3c8f87b
MobbPanda <al.astudilloh@duocuc.cl>
2025-12-04 22:32:18 -0300

    Se agrega el CrossOrigin al ProductoController

--- usInventario/src/main/java/com/petsocity/petsocity/controller/ProductoController.java
+++ usInventario/src/main/java/com/petsocity/petsocity/controller/ProductoController.java
@@ -14,10 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.petsocity.petsocity.model.Producto;
 import com.petsocity.petsocity.service.ProductoService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/productos")
 public class ProductoController {
